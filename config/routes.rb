c90c65f31d495e95c9a035156f83cb0063b1cfa5
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-19 16:26:28 +0800

    added controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  root to: "pages#home"
+
+  resources :sections, only: [:index] do
+    resources :chats, only: [:create]
+  end
+
+  resources :chats, only: [:show] do
+    resources :messages, only: [:create]
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
