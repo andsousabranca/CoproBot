a260d6cd2fe811169f5bdba7a2c312456290a991
Pierre Keane <Pierrekeane@gmail.com>
2025-11-20 15:09:47 +0800

    coded the create method of the ChatsController and linked some routing.

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,15 @@
 class ChatsController < ApplicationController
 
+  def create
+    @section = Section.find(params[:section_id])
+    @chat = Chat.new(title: @section.name, section_id: @section.id, user_id: 1)
+
+    if @chat.save
+      redirect_to chat_path(@chat)
+    end
+  end
+
+
     def show
     @chat = Chat.find(params[:id])
     @section = @chat.section
@@ -9,5 +19,9 @@ class ChatsController < ApplicationController
     # get all the current and historic chats of the section
     @chats = @section.chats.order(created_at: :desc)
   end
-  
+
+  def chat_params
+    params.require(:chat).permit(:title)
+  end
+
 end
