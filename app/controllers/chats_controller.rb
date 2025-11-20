58da4d1554c91b19f02cb5a194324ed0681e9218
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-20 16:45:05 +0800

    added turbo and updated show page

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -13,8 +13,7 @@ class ChatsController < ApplicationController
     def show
     @chat = Chat.find(params[:id])
     @section = @chat.section
-    @user = @chat.user
-    @message = Message.new()
+    @message = Message.new
 
     # get all the current and historic chats of the section
     @chats = @section.chats.order(created_at: :desc)
@@ -22,19 +21,19 @@ class ChatsController < ApplicationController
 
   def chat_params
     params.require(:chat).permit(:title)
-  
-  def create
-    @section = Section.find_by(params[:id])
-
-    # initialize @chat variable with last chat
-    @chat = Chat.last
-    
-    # check if that chat alredy has some messages, if yes, then it is a used chat, hence we have to create a new one
-    if @chat.messages.any?
-      @chat = Chat.create!(user: current_user, section: @section, title: "New chat")
-    end
-
-    raise
   end
+  
+  # def create
+  #   @section = Section.find_by(params[:id])
+
+  #   # initialize @chat variable with last chat
+  #   @chat = Chat.last
+    
+  #   # check if that chat alredy has some messages, if yes, then it is a used chat, hence we have to create a new one
+  #   if @chat.messages.any?
+  #     @chat = Chat.create!(user: current_user, section: @section, title: "New chat")
+  #   end
+
+  # end
 
 end
