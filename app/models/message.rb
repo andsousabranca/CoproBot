45cc987031a1f4a6467645db31710fba4ed66018
Andreas Sousa Branca <andreaslsb@gmail.com>
2025-11-21 14:59:38 +0800

    finalized turbo implementation

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,16 @@ class Message < ApplicationRecord
   belongs_to :chat
 
   validates :role, presence: :true
-  validates :content, presence: :true
+  # validates :content, presence: :true
+
+  after_create_commit :broadcast_append_to_chat
+
+  private
+
+  def broadcast_append_to_chat
+    broadcast_append_to chat, 
+      target: "messages", 
+      partial: "messages/message", 
+      locals: { message: self }
+  end
 end
